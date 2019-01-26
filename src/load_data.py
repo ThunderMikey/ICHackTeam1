16ac7e74a9fe5c0f346b2a70144e62e079ad1e8e
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2019-01-26 20:10:34 +0000

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -5,14 +5,14 @@ import mongodbutil as ml
 dbhost='localhost'
 dbclient=ml.mongoclient(dbhost)
 
-df1=pd.read_csv('data/min_temperature.csv')
+df1=pd.read_csv('data/min_temperature.csv',sep='\t')
 ml.df2mongo(dbclient,'USweather','mintemperature',df1)
-df2=pd.read_csv('data/max_temperature.csv')
+df2=pd.read_csv('data/max_temperature.csv',sep='\t')
 ml.df2mongo(dbclient,'USweather','maxtemperature',df2)
-df3=pd.read_csv('data/max_precipitation.csv')
+df3=pd.read_csv('data/max_precipitation.csv',sep='\t')
 ml.df2mongo(dbclient,'USweather','maxprecipitation',df3)
-df4=pd.read_csv('data/mean_precipitation.csv')
+df4=pd.read_csv('data/mean_precipitation.csv',sep='\t')
 ml.df2mongo(dbclient,'USweather','meanprecipitation',df4)
-df5=pd.read_csv('data/countystate.csv')
+df5=pd.read_csv('data/countystate.csv',sep='\t')
 ml.df2mongo(dbclient,'USweather','countystate',df5)
 
