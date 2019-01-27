225c94ad0b2164bb349d8cd790ec78d6249e43a4
harrisonzhu508 <harrisonzhu5080@gmail.com>
2019-01-27 07:52:12 +0000

    tseries

--- src/model/tseries.R
+++ src/model/tseries.R
@@ -1,25 +1,35 @@
-# forecasts spacetime temperatures
-library(dbarts)
+library(forecast)
+library(tseries)
+library(doParallel)
 
 trainData <- read.csv("../data/max_precipitation.csv", sep="\t")
-trainData <- trainData[trainData$Year > 2005, ]
-dim <- 3
+num_obs <- length(trainData[trainData["Year"] == 2006,1])
 
-testData <- cbind(Year = 2018, trainData[trainData$Year == 2006,c(2,3)])
-for (year in 2019:2025) {
-  testData <- rbind(cbind(Year=year, trainData[trainData$Year == 2006,c(2,3)]), testData)
+year2006_data <- trainData[trainData["Year"] == 2006,]
+
+cl <- makeCluster(4)
+registerDoParallel(cl)
+
+predictions <- foreach(i=1:num_obs, .combine = rbind, .packages = c("forecast", "tseries")) %dopar% { 
+  
+  latitude <- year2006_data[i,2]
+  longitude <- year2006_data[i,3]
+  
+  index <- intersect(which(c(trainData[,c(2)] == latitude)), which(c(trainData[,c(3)] == longitude)))
+  #fit AR2 models
+  fit <- arima(trainData[index, 4], order = c(2,0,0))
+  y_pred <- forecast(fit, h = 8)$mean
+  pred_val <- c(y_pred[1], latitude, longitude, 2018)
+  
+  for (j in 2:8) {
+    pred_val <- rbind(c(y_pred[j], latitude, longitude, (2017+j)), pred_val)
+  }
+  print(pred_val)
 }
 
-model <- bart(trainData[,1:dim], trainData[,dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 2)
+stopCluster(cl)
 
-y_pred <- colMeans(predict(model, testData[,c(1,2,3)]))
+predictions <- data.frame(predictions)
+colnames(predictions) <- c("maxprecipitatio", "Latitude", "Longitude", "Year")
 
-predictions <- data.frame(
-    "Year"<- c(testData[, 1]),
-    "Latitude" <- c(testData[, 2]),
-    "Longitude" = c(testData[, 3]),
-    "prediction"= y_pred
-)
-colnames(predictions) <- c("Year", "Latitude", "Longitude", "maxprecipitation")
-
-write.csv("../data/prediction_maxprecipitation.csv", x = predictions)
+write.csv("../data/prediction_maxprecipitatio_time.csv", x = predictions, row.names=FALSE)
