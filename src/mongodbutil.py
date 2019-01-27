74f85ba5cf72150bfc6eb725049a41ab6357e8d0
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2019-01-27 00:19:33 +0000

    Update mongodbutil.py

--- src/mongodbutil.py
+++ src/mongodbutil.py
@@ -137,6 +137,52 @@ def create_time_series(dff,x_col,y_col,title, axis_type='Linear'):
         }
     }
 
+def generate_spatial(df,graphtitle):
+    scl = [ [0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],
+    [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"] ]
+    data = [ dict(
+            type = 'scattergeo',
+            locationmode = 'USA-states',
+            lon = df['Longitude'],
+            lat = df['Latitude'],
+            text = df['text'],
+            mode = 'markers',
+            marker = dict(
+                size = 8,
+                opacity = 0.8,
+                reversescale = True,
+                autocolorscale = False,
+                symbol = 'square',
+                line = dict(
+                    width=1,
+                    color='rgba(102, 102, 102)'
+                ),
+                colorscale = scl,
+                cmin = 0,
+                color = df['cnt'],
+                cmax = df['cnt'].max(),
+                colorbar=dict(
+                    title="Incoming flightsFebruary 2011"
+                )
+            ))]
+
+    layout = dict(
+            title = graphtitle,
+            colorbar = True,
+            geo = dict(
+                scope='usa',
+                projection=dict( type='albers usa' ),
+                showland = True,
+                landcolor = "rgb(250, 250, 250)",
+                subunitcolor = "rgb(217, 217, 217)",
+                countrycolor = "rgb(217, 217, 217)",
+                countrywidth = 0.5,
+                subunitwidth = 0.5
+            ),
+        )
+    fig = dict(data=data,layout=layout) 
+    return fig
+
 if __name__=='__main__':
     client=mongoclient('localhost')
     df=generate_historical_data(client,'California','Imperial','meanprecipitation')
