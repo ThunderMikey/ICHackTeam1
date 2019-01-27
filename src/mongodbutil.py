f8d3569c7f15384e291047dc7225618889ed378f
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2019-01-27 10:08:22 +0000

    Update mongodbutil.py

--- src/mongodbutil.py
+++ src/mongodbutil.py
@@ -177,7 +177,7 @@ def addunit_metric(metric):
     return newmetric
 
 def generate_spatial(df,graphtitle,metric):
-    df['text'] = df['state_fullname']+' '+df['County']+' '+df[metric].astype(str)
+    
     scl=[[0.0, 'rgb(165,0,38)'], [0.1111111111111111, 'rgb(215,48,39)'], [0.2222222222222222, 'rgb(244,109,67)'], 
     [0.3333333333333333, 'rgb(253,174,97)'], [0.4444444444444444, 'rgb(254,224,144)'], 
     [0.5555555555555556, 'rgb(224,243,248)'], [0.6666666666666666, 'rgb(171,217,233)'], 
@@ -185,6 +185,8 @@ def generate_spatial(df,graphtitle,metric):
      [0.8888888888888888, 'rgb(69,117,180)'], [1.0, 'rgb(49,54,149)']]
     if metric=='maxtemperature' or metric=='mintemperature':
         df[metric]=df[metric]-273.15
+
+    df['text'] = df['state_fullname']+' '+df['County']+' '+df[metric].astype(str)
     data = [ dict(
             type = 'scattergeo',
             locationmode = 'USA-states',
